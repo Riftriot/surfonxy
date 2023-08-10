2c61104de8df0c925fc2f7d176a4750557446a05
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-10 09:28:16 +0200

    fix(worker): resolve URL issues by checking every single URL string
    - better check for already patched requests
    - prevent `/` URLs to have an url parameter pointing to the proxy's origin
    - fix `duplex` issue with ReadableStream body by cloning and reading the body's `text` and then passing it to the new patch request

--- src/client/worker.ts
+++ src/client/worker.ts
@@ -5,15 +5,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 import { SURFONXY_URI_ATTRIBUTES } from "../utils/constants";
 
-
-/** Origin to replace with in patched requests. */
-let proxyOrigin: string;
-let currentlyProxyingURL: URL;
-
 self.addEventListener("install", () => {
-  console.info("[service-worker]: installing...");
-  proxyOrigin = new URL(location.href).origin;
-  
   self.skipWaiting();
   console.info("[service-worker]: installed !");
 });
@@ -31,47 +23,65 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+let previous_client_url: string;
+
 self.addEventListener("fetch", (event) => {
   event.stopPropagation();
   event.stopImmediatePropagation();
 
   const original_request = event.request;
+  const sendOriginalRequest = () => fetch(original_request);
 
   try {
     event.respondWith((async () => {
-      const original_url = new URL(original_request.url);
+      /** URL in the fetch. */
+      const original_request_url = new URL(original_request.url);
       const client = await self.clients.get(event.clientId);
 
-      if (client?.url) {
-        const url = new URL(client.url);
-        currentlyProxyingURL = new URL(url.pathname + url.search,
-          atob(url.searchParams.get(SURFONXY_URI_ATTRIBUTES.URL) as string)
-        );
+      if (!client?.url && !previous_client_url) return sendOriginalRequest();
+      previous_client_url = (client?.url ?? previous_client_url as string);
+      const client_url = new URL(previous_client_url);
+
+      const proxy_origin_encoded = client_url.searchParams.get(SURFONXY_URI_ATTRIBUTES.URL);
+      if (!proxy_origin_encoded) return sendOriginalRequest();
+
+      let proxy_origin_decoded: string;
+      try {
+        // We wrap it in a try/catch to "catch" decoding errors.
+        proxy_origin_decoded = atob(decodeURIComponent(proxy_origin_encoded));
       }
+      catch { return sendOriginalRequest(); }
     
-      // We leave the request untouched when it's something we don't need.
-      if (original_url.protocol === "chrome-extension:" || original_url.search.includes(SURFONXY_URI_ATTRIBUTES.URL)) {
-        return fetch(original_request);
+      // We only handle `http` and `https` requests, for now.
+      if (!original_request_url.protocol.startsWith("http")) return sendOriginalRequest();
+      
+      // We ignore already patched requests.
+      if (original_request_url.searchParams.get(SURFONXY_URI_ATTRIBUTES.URL)) {
+        // Only ignore if the origin was patched as well.
+        if (original_request_url.origin === client_url.origin) return sendOriginalRequest();
       }
 
       // When the origin is the same, that means that it should be on the `currentlyProxyingURL` origin.
-      const patched_origin = original_url.origin === proxyOrigin
-        ? currentlyProxyingURL.origin
-        : original_url.origin
+      const patched_origin = original_request_url.origin === client_url.origin
+        ? proxy_origin_decoded
+        : original_request_url.origin
     
-      const patched_url = new URL(original_url.pathname + original_url.search, proxyOrigin);
+      const patched_url = new URL(original_request_url.pathname + original_request_url.search + original_request_url.hash, client_url.origin);
       patched_url.searchParams.set(SURFONXY_URI_ATTRIBUTES.URL, btoa(patched_origin))
       patched_url.searchParams.set(SURFONXY_URI_ATTRIBUTES.READY, "1"); // Always "1" since SW is installed !
-    
+
+      const patched_request_body = await original_request.clone().text();
+
       const patched_request = new Request(patched_url, {
-        body: original_request.body,
+        body: (original_request.method !== "GET" && original_request.method !== "HEAD" && patched_request_body) ? patched_request_body : void 0,
         method: original_request.method,
         headers: original_request.headers,
         redirect: original_request.redirect,
-        credentials: original_request.credentials,
+        // mode: "cors",
+        // cache: "default",
+        // credentials: "include"
       });
     
-      // console.info("[service-worker]:", "original", original_request, "patched", patched_request);
       return fetch(patched_request);
     })());
   }
