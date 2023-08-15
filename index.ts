d370907d4ae754f03de40e73b158e24abbae25cd
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-15 22:58:29 +0200

    fix(demo): use proper types for elysia

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia, ws, type Context } from "elysia";
+import type { WebSocketHandler } from "bun";
 
-import { createProxiedResponse, createSession } from "./src";
+import { createProxiedResponse, createSession, makeProxyWebSocketHandler } from "./src";
 const session = createSession();
 
+const WEBSOCKET_BASE_PATH = "/__surfonxy_websocket__";
+
 new Elysia()
-  /** `:url` should be encoded in `base64`. */
+  .use(ws())
+  .ws(WEBSOCKET_BASE_PATH + "/*", makeProxyWebSocketHandler(WEBSOCKET_BASE_PATH) as Omit<Partial<WebSocketHandler<Context>>, "publish" | "open" | "message" | "close" | "drain" | "publishToSelf">)
   .all("*", ({ request }) => {
-    return createProxiedResponse(request, session);
+    return createProxiedResponse(request, session, {
+      WEBSOCKET_PROXY_PATH: WEBSOCKET_BASE_PATH
+    });
   })
   .listen(8000, (server) => {
     console.info(`running on ${server.hostname}:${server.port}`);
