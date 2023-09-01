e384d6a3438a8810177ad2c1d566d1c3a2ce8ec2
Mikkel RINGAUD <mikkel@milescode.dev>
2023-09-01 14:07:19 +0200

    fix: rewrite `tweakJS` using new deps

--- src/proxy/tweaks/javascript.ts
+++ src/proxy/tweaks/javascript.ts
@@ -1,6 +1,6 @@
-import { parse } from "@babel/parser";
-import traverse from "@babel/traverse";
-import generate from "@babel/generator";
+import { parse } from "meriyah";
+import { traverse } from "estree-toolkit";
+import { generate } from "astring";
 
 const WINDOW_LOCATION_TWEAKED_PROPERTY = "__sf_location";
 
@@ -10,35 +10,44 @@ const WINDOW_LOCATION_TWEAKED_PROPERTY = "__sf_location";
  */
 export const tweakJS = (code: string): string => {
   const ast = parse(code, {
-    sourceType: "unambiguous",
+    module: true
   });
-  
+
   traverse(ast, {
-    enter (path) {
-      if (path.node.type === "MemberExpression") {
-        if (path.node.object.type === "Identifier") {
-          let from = path.node.object.name;
-          if (from !== "window") {
-            let bind = path.scope.getBinding(from);
-            if (!bind) return;
+    $: { scope: true },
+    MemberExpression (path) {
+      if (!path.node) return;
 
-            if (bind.path.node.type === "VariableDeclarator" && bind.path.node.init && bind.path.node.init.type === "Identifier") {
-              from = bind.path.node.init.name;
-            } else return;
+      if (path.node.object.type === "Identifier") {
+        if (!path.scope) return;
 
-            if (from !== "window") return;
-          }
+        let object_name = path.node.object.name;
 
-          if (path.node.property.type === "Identifier" && path.node.property.name === "location") {
-            // only if the window variable was not redefined before
-            if (path.scope.hasBinding("window")) return;
+        if (object_name === "location") {
+          path.node.object.name = WINDOW_LOCATION_TWEAKED_PROPERTY;
+          return;
+        }
+        
+        if (object_name !== "window") {
+          let bind = path.scope.getBinding(object_name);
+          if (!bind) return;
 
-            path.node.property.name = WINDOW_LOCATION_TWEAKED_PROPERTY;
-          }
+          if (bind.path.node && bind.path.node.type === "VariableDeclarator" && bind.path.node.init && bind.path.node.init.type === "Identifier") {
+            object_name = bind.path.node.init.name;
+          } else return;
+
+          if (object_name !== "window") return;
+        }
+
+        if (path.node.property.type === "Identifier" && path.node.property.name === "location") {
+          // only if the window variable was not redefined before
+          if (path.scope.hasBinding("window")) return;
+          
+          path.node.property.name = WINDOW_LOCATION_TWEAKED_PROPERTY;
         }
       }
     }
   });
 
-  return generate(ast, /*{ minified: true }*/).code;
+  return generate(ast);
 };
